c113cee0570fb9aa6a41f1a5f59a04d086406ad7
takashiono2 <takashiono710@gmail.com>
2023-04-19 01:39:31 +0900

    section4_29

---
+++ section4/04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -0,0 +1,21 @@
+import "./Expression.css"
+const Expression = () => {
+  const title = "Expression";
+  const arry = ['item1','item2','item3'];
+  const hello = (arg) => {
+    return `${arg} Function`;
+  }
+  const jsx = <h3>Hello JSX</h3>;
+
+  return(
+    // <div className="expression">
+    <div className={title.toLowerCase()}>
+      <h3>Hello {title}</h3>
+      <h3>{arry}</h3>
+      <h3>{ hello('Hello') }</h3>
+      { <h3>Hello JSX</h3> }
+      { jsx }
+    </div>
+  )
+}
+export default Expression;
