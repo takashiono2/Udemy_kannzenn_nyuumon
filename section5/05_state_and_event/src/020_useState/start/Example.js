77ac449b53525c1199cd181108097a00b5a89975
takashiono2 <takashiono710@gmail.com>
2023-04-21 02:02:49 +0900

    seciton5_43

--- section5/05_state_and_event/src/020_useState/start/Example.js
+++ section5/05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 const Example = () => {
+  let [val, setVal] = useState(0);
 
   return (
     <>
-      
+      <input type="text"
+        onChange={(e) => {
+          // const setFn = valArry[1];
+          setVal(e.target.value)
+        }}
+      /> = {val}
     </>
   );
 };
