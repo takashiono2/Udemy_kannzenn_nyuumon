3fed1e1c32dd13fb2706fb52b6709aa054441b6e
takashiono2 <takashiono710@gmail.com>
2023-04-25 22:44:37 +0900

    seciton6_66

--- section6/06_control_and_form/src/080_input_textarea/start/Example.js
+++ section6/06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 const Example = () => {
+  const [val, setVal] = useState("");
+  const clearVal = () => {
+    setVal("");
+  }
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+  <>
+    <label htmlFor="456">ラベル</label>
+    <>
+      <input
+        id="123"
+        placeholder="こんにちは"
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+      />``
+      <textarea
+        id="456"
+        value={val}
+        placeholder="こんにちは"
+        onChange={(e) => setVal(e.target.value)}
+      />
+    </>
+      <h3>{val}</h3>
+      <button onClick={clearVal}>クリア</button>
+  </>
   );
 };
-
 export default Example;
--- section6/06_control_and_form/src/090_radio/start/Example.js
+++ section6/06_control_and_form/src/090_radio/start/Example.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
 const Example = () => {
+  const[fruit, setFruit] = useState("Apple");
+  const onChange = (e) => {
+    setFruit(e.target.value);
+  }
+  const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+  <>
+    {RADIO_COLLECTION.map((value)=> {
+      return(
+        <label key={value}>
+          <input
+            type="radio"
+            checked={fruit===value}
+            onChange={onChange}
+            value={value}
+          />
+          {value}
+        </label>
+      )
+    })}
+    <h3>私は{fruit}が食べたい</h3>
+  </>
   );
 };
 
