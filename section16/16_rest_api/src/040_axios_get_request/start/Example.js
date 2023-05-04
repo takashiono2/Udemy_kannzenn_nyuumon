3a403bafb2da1841c376b868163b92cbd9c85689
takashiono2 <takashiono710@gmail.com>
2023-05-04 20:13:19 +0900

    section16_173

--- section16/16_rest_api/src/040_axios_get_request/start/Example.js
+++ section16/16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,5 +1,29 @@
-const Example = () => {
-  return <></>;
-};
+import { useEffect,useState } from "react";
 
+import axios from "axios";
+//usersをmap関数で取得
+const Example = () => {
+  const [users,setUsers] = useState([]);
+  useEffect(() =>{
+    const getUser = async () =>{
+      const res = await axios.get('http://localhost:3003/user');
+      setUsers(res.data);
+    }
+    getUser();
+  },[])
+
+  return(
+  <>
+    {users?.map((user) => {
+      return(
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>age: {user.age}</p>
+          <p>hobby: {user.hobbies.join(',')}</p>
+        </div>
+      )
+    })}
+  </>
+  )
+};
 export default Example;
