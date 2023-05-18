36174882c30b32a45e91b18bfd81da3022884234
takashiono2 <takashiono710@gmail.com>
2023-05-19 01:25:51 +0900

    section18_204

--- section18/18_nextjs_p2/start/src/pages/020_SG/[id].js
+++ section18/18_nextjs_p2/start/src/pages/020_SG/[id].js
@@ -1,5 +1,11 @@
-export default function Page({ id }){
-  return<h3>このページは{id}です。</h3>
+import { useRouter } from "next/router";
+
+export default function Page({ id,date }){
+  const router = useRouter();
+  if(router.isFallback){
+    return <h3>loadConfig...</h3>
+  }
+  return<h3>このページは{id}です。{date}</h3>
 }
 
 export async function getStaticPaths(){
@@ -7,14 +13,17 @@ export async function getStaticPaths(){
     paths:[
       { params: { id: "1" } },{ params: { id: "2" } }
     ],
-    fallback: false
+    fallback: "blocking",
   };
 }
 
 export async function getStaticProps({params}){
+  const date = new Date;
   return{
     props: {
-      id: params.id
-    }
-  }
+      id: params.id,
+      date: date.toJSON()
+    },
+    revalidate: 5
+  };
 }
